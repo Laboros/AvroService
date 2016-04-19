e330b6c8088a00b1b0a4ae536b9658ac0036e383
svaduka <sainagaraju.vaduka@gmail.com>
2016-04-19 21:16:46 +0530

    Updated .gitIgnore and written logic for AvroStorageFormat

--- AvroService/src/main/java/org/avroservice/AvroStorageFormat.java
+++ AvroService/src/main/java/org/avroservice/AvroStorageFormat.java
@@ -1,7 +1,12 @@
 package org.avroservice;
 
+import java.util.List;
+
 import org.apache.avro.Schema;
+import org.apache.avro.Schema.Field;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.io.WritableComparable;
 import org.laboros.StorageFormat;
 
@@ -46,18 +51,40 @@ public class AvroStorageFormat implements StorageFormat {
 		this.isFixedWidth = isFixedWidth;
 	}
 
-	public WritableComparable<GenericRecord> parseData(String inputData) {
+	public Object parseData(String inputData) {
 		
 		return null;
 	}
 
-	public WritableComparable<GenericRecord> parseFixedWidthData(final String inputData)
+	public GenericRecord parseFixedWidthData(final String inputData)
 	{
 		return null;
 	}
 	
-	public WritableComparable<GenericRecord> parseDelimitedData(final String inputData){
-		return null;
+	public GenericRecord parseDelimitedData(final String inputData)
+	{
+		GenericRecord record=null;
+		if(getDelimiter()!=null)
+		{
+
+			final String delimiter=getDelimiter();
+			
+			GenericRecordBuilder builder=new GenericRecordBuilder(oSchema);
+			record=builder.build();;
+			List<Field> fields=oSchema.getFields();
+
+			String[] columns=StringUtils.splitPreserveAllTokens(delimiter);
+			
+			for (int i = 0; i < fields.size(); i++) 
+			{
+				if(i<=columns.length)
+				{
+				record.put(i, columns[i]);
+				}
+			}
+	
+			}
+		return record;
 	}
 	
 
