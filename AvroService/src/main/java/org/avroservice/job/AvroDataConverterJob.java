4e98223d9e0da499164150ca24fdc5e53df9e4e4
svaduka <sainagaraju.vaduka@gmail.com>
2016-04-21 21:40:38 +0530

    Updated pom.xml for AvroKeyValueOutputFormat

--- AvroService/src/main/java/org/avroservice/job/AvroDataConverterJob.java
+++ AvroService/src/main/java/org/avroservice/job/AvroDataConverterJob.java
@@ -1,10 +1,13 @@
 package org.avroservice.job;
 
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.mapreduce.AvroKeyValueOutputFormat;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.avroservice.mapper.AvroDataConverterMapper;
@@ -15,16 +18,29 @@ public class AvroDataConverterJob extends Configured implements Tool {
 	public int run(String[] args) throws Exception {
 		
 		Configuration conf = super.getConf();
+		
 		Job avroDataConverterJob = Job.getInstance(conf, this.getClass().getName());
+		
 		avroDataConverterJob.setJarByClass(AvroDataConverterJob.class);
 
-		FileInputFormat.addInputPath(avroDataConverterJob, new Path(args[0]));
+		avroDataConverterJob.setInputFormatClass(TextInputFormat.class);
+		TextInputFormat.addInputPath(avroDataConverterJob, new Path(args[0]));
+		
+		avroDataConverterJob.setMapOutputKeyClass(GenericRecord.class);
+		avroDataConverterJob.setMapOutputValueClass(NullWritable.class);
+		
+		avroDataConverterJob.setOutputFormatClass(AvroKeyValueOutputFormat.class);
+		
+		avroDataConverterJob.setOutputKeyClass(GenericRecord.class);
+		avroDataConverterJob.setOutputValueClass(NullWritable.class);
+		
 		FileOutputFormat.setOutputPath(avroDataConverterJob, new Path(args[1]));
 		
 		avroDataConverterJob.setMapperClass(AvroDataConverterMapper.class);
 		avroDataConverterJob.setReducerClass(AvroDataConverterReducer.class);
 		
 		avroDataConverterJob.submit();
+		
 		return 0;
 	}
 }
